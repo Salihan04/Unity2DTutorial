4b0ebe719bc4c9bbe61c2e3aca2fe6ccd6d1120c
Salihan04 <salihan04@gmail.com>
2014-12-10 16:14:09 +0800

    Added jump animations to RobotController animator and link it to RobotControllerScript. Added jumping to RobotControllerScript

--- Assets/Scripts/RobotControllerScript.cs
+++ Assets/Scripts/RobotControllerScript.cs
@@ -6,7 +6,15 @@ public class RobotControllerScript : MonoBehaviour {
 	public float maxSpeed = 10;
 	private bool facingRight = true;
 
-	Animator anim;
+	private Animator anim;
+
+	private bool grounded = false;
+	public Transform groundCheck;
+	private float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 700f;
+
+	private bool doubleJump = false;
 
 	// Use this for initialization
 	void Start () 
@@ -17,6 +25,17 @@ public class RobotControllerScript : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+		anim.SetBool("Ground", grounded);
+
+		if(grounded)
+			doubleJump = false;
+
+		anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
+
+		if(!grounded)
+			return;
+
 		float move = Input.GetAxis("Horizontal");
 
 		anim.SetFloat("Speed", Mathf.Abs(move));
@@ -27,6 +46,18 @@ public class RobotControllerScript : MonoBehaviour {
 			Flip();
 		else if(move < 0 && facingRight)
 			Flip ();
+	}
+
+	void Update()
+	{
+		if((grounded || !doubleJump) && Input.GetKeyDown(KeyCode.Space))
+		{
+			anim.SetBool("Grounded", false);
+			rigidbody2D.AddForce(new Vector2(0, jumpForce));
+
+			if(!doubleJump && !grounded)
+				doubleJump = true;
+		}
 	}
 
 	void Flip() 
